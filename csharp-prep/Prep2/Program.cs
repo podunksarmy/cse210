512fe5f73032c5c1744143b24d97c369d7c05bdc
podunksarmy <podnksarmy@gmail.com>
2024-04-27 17:17:41 -0600

    Added if/then clause to determine letter grade & pass/fail

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade? ");
+        string userInput = Console.ReadLine();
+        int userGrade = int.Parse(userInput);
+
+        if (userGrade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (userGrade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (userGrade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (userGrade >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+        int passFail = userGrade;
+
+        if (passFail >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sucks to suck. Better luck next time!");
+        }
+    }   
 }
