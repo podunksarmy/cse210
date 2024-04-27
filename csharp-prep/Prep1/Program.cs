e0a77fa07971141dce39817855c3952f31ea6341
podunksarmy <podnksarmy@gmail.com>
2024-04-27 15:06:21 -0600

    "fixed variables to came case"

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,10 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your first name?");
-        string firstname = Console.ReadLine();
+        string firstName = Console.ReadLine();
         
         Console.WriteLine("What is your last name?");
-        string lastname = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
