cab4a60b6f5e23f5b00de9349b9288d80bacf573
podunksarmy <podnksarmy@gmail.com>
2024-04-30 22:11:05 -0600

    Got the first leg of the program running

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        string userNumber = Console.ReadLine();
+        int magicNumber = int.Parse(userNumber);
+
+        Console.Write("Guess a number: ");
+        string userGuess = Console.ReadLine();
+        int magicGuess = int.Parse(userGuess);
+
+        if (magicGuess == magicNumber)
+        {
+            Console.WriteLine("Congratulations!  You guessed it!");
+        }
+        else if (magicGuess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (magicGuess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
     }
 }
