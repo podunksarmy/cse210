0940e48ca0a5fb5e814ff3bd8f5fa5b5e47a69bf
podunksarmy <podnksarmy@gmail.com>
2024-05-03 17:51:38 -0600

    nested the game in a while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,25 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is the magic number?");
-        string userNumber = Console.ReadLine();
-        int magicNumber = int.Parse(userNumber);
+        string response = "yes";
+        while (response == "yes")
+        {       
+            Console.WriteLine("What is the magic number?");
+            string userNumber = Console.ReadLine();
+            int magicNumber = int.Parse(userNumber);
 
-        Console.Write("Guess a number: ");
-        string userGuess = Console.ReadLine();
-        int magicGuess = int.Parse(userGuess);
+            Console.Write("Guess a number: ");
+            string userGuess = Console.ReadLine();
+            int magicGuess = int.Parse(userGuess);
 
-        if (magicGuess == magicNumber)
-        {
-            Console.WriteLine("Congratulations!  You guessed it!");
-        }
-        else if (magicGuess > magicNumber)
-        {
-            Console.WriteLine("Lower");
-        }
-        else if (magicGuess < magicNumber)
-        {
-            Console.WriteLine("Higher");
+            if (magicGuess == magicNumber)
+            {
+                Console.WriteLine("Congratulations!  You guessed it!");
+            }
+            else if (magicGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
         }
     }
 }
