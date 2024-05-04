f4b53e9584935b098f458f99faaf86960922edb5
podunksarmy <podnksarmy@gmail.com>
2024-05-03 22:29:38 -0600

    did my best

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+       DisplayWelcome();
+
+       string userName = PromptUserName();
+       int favoriteNumber = PromptUserNumber();
+
+       int squaredNumber = SquareNumber(favoriteNumber);
+    }
+
+    static void DisplayWelcome()
+       {
+           Console.WriteLine("Welcome to the Program");
+       }
+
+    static PromptUserName(string userName)
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        
+        return name;
+    }
+
+    static int PromptUserNumber(int userNumber)
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int userNumber)
+    {
+        int square = userNumber * userNumber;
+
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}")
     }
 }
