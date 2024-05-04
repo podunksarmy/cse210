251d11c86305a6a0c75da7d150d8721c41dde3ef
podunksarmy <podnksarmy@gmail.com>
2024-05-03 18:24:07 -0600

    fixed code to be more eefficient

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,31 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        string response = "yes";
-        while (response == "yes")
-        {       
-            Console.WriteLine("What is the magic number?");
-            string userNumber = Console.ReadLine();
-            int magicNumber = int.Parse(userNumber);
+    //     string response = "yes";
+    //     while (response == "yes")
+    //     {       
+    //         Console.WriteLine("What is the magic number?");
+    //         string userNumber = Console.ReadLine();
+    //         int magicNumber = int.Parse(userNumber);
 
-            Console.Write("Guess a number: ");
-            string userGuess = Console.ReadLine();
-            int magicGuess = int.Parse(userGuess);
+    //         Console.Write("Guess a number: ");
+    //         string userGuess = Console.ReadLine();
+    //         int magicGuess = int.Parse(userGuess);
 
-            if (magicGuess == magicNumber)
+    //         if (magicGuess == magicNumber)
+    //         {
+    //             Console.WriteLine("Congratulations!  You guessed it!");
+    //         }
+    //         else if (magicGuess > magicNumber)
+    //         {
+    //             Console.WriteLine("Lower");
+    //         }
+    //         else if (magicGuess < magicNumber)
+    //         {
+    //             Console.WriteLine("Higher");
+    //         }
+    //         Console.Write("Do you want to continue? ");
+    //         response = Console.ReadLine();
+    //     }
+    // 
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 101);
+
+        int response = -1;
+
+        
+        while (response != magicNumber)
+        {
+            Console.WriteLine("Choose a number between 0 and 9999? ");
+            response = int.Parse(Console.ReadLine());
+
+            if (magicNumber < response)
             {
-                Console.WriteLine("Congratulations!  You guessed it!");
+                Console.WriteLine("Too high! Guess again: ");
             }
-            else if (magicGuess > magicNumber)
+
+            else if (magicNumber > response)
             {
-                Console.WriteLine("Lower");
+                Console.WriteLine("Too low! Try again: ");
             }
-            else if (magicGuess < magicNumber)
+            else
             {
-                Console.WriteLine("Higher");
+                Console.WriteLine("You guessed it!");
             }
-            Console.Write("Do you want to continue? ");
-            response = Console.ReadLine();
+    
         }
     }
 }
