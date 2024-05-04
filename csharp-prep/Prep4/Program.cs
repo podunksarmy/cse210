d451b2d3e25f4f3e0cf22c4a5e5b4da0d77ac93f
podunksarmy <podnksarmy@gmail.com>
2024-05-03 19:23:18 -0600

    list that adds items until 0 is input

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,23 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+        
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
     }
 }
